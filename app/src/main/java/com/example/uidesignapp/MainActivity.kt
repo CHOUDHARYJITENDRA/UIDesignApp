a9157f550f10bf0d6e9df5cc015dd847a9a6c5cd
JK JAAT <choudharyjitendra000@gmail.com>
2025-06-26 15:24:44 +0530

    Initial commit - UIDesignApp

---
+++ app/src/main/java/com/example/uidesignapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.uidesignapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.bottom_nav)
+
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+            bottomNav.selectedItemId = R.id.nav_home
+        }
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> loadFragment(HomeFragment())
+                R.id.nav_voucher -> loadFragment(VoucherFragment())
+                R.id.nav_order -> loadFragment(OrderFragment())
+                R.id.nav_account -> loadFragment(AccountFragment())
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
